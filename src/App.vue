365ec2f951edc90c8b3a06ab15cb5435f211d048
VOxOVb <147232441+VOxOVb@users.noreply.github.com>
2025-01-14 16:14:07 +0800

    feat: import component in App

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+import GoogleSignIn from './components/GoogleSignIn.vue'
+import Map from './components/Map.vue'
+
+const userInfo = ref(null);
+const updateUserInfo = (data) => {
+    userInfo.value = data;
+};
+const localUserInfo = localStorage.getItem('userInfo');
+if (localUserInfo) updateUserInfo(JSON.parse(localUserInfo))
+</script>
+
+<template>
+    <Map v-if="userInfo" />
+    <GoogleSignIn v-else @user-logged-in="updateUserInfo" />
+</template>
+
+<style scoped></style>
